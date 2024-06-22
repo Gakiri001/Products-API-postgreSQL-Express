4a4a2b2bf50dd92e764be6560b06ce718cf61d18
Gakiri001 <gakirijulius@gmail.com>
2024-06-22 17:05:53 +0300

    implemented the GET a single product response and the error message for the response

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -9,3 +9,19 @@ export const getALLProducts = async(req,res) => {
     res.status(500).json({success:false,message:err.message})
   }
 }
+
+export const getSingleProduct = async (req,res) => {
+  const id = req.params.id
+  try{
+    const result = await pool.query("SELECT * FROM products WHERE id=$1",[id,])
+    if(result.rowCount===1){
+      res.status(200).json({success:true, data:result.rows[0]})
+    }
+    else{
+      res.status(400).json({success:false,message:"Product not found"})
+    }
+  }
+    catch(err){
+      res.status(500).json({success:false, message:err.message})
+    }
+}
--- src/route/products.route.js
+++ src/route/products.route.js
@@ -1,14 +1,12 @@
 import {Router} from "express"
 // import pool from "../db.config.js";
-import { getALLProducts } from "../controllers/products.controllers.js"; 
+import { getALLProducts,getSingleProduct } from "../controllers/products.controllers.js"; 
 
 const router = Router();
 
 router.get("/", getALLProducts )
 
-router.get("/:id",(req,res) => {
-  res.send("Gets a single product")
-})
+router.get("/:id",getSingleProduct)
 
 router.post("/",(req,res) => {
   res.send("Adds/Creates a new product to the database")
